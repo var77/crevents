69e8804895e341964a94aa26355cb10cce90d168
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-03-08 13:55:39 +0400

    feat(refactor): refactor with nx monorepo

---
+++ packages/events-ui/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="events-ui" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
---
+++ packages/smart-contracts/test/index.test.ts
@@ -0,0 +1,56 @@
+import { ethers } from "hardhat";
+
+const defaultEvent = {
+  name: "Test event",
+  description: "test description",
+  link: "https://example.com",
+  maxParticipants: 10,
+  registrationEnd: Date.now() + 100000,
+  start: Date.now() + 200000,
+  end: Date.now() + 40000,
+  ticketPrice: 10000,
+  preSaleTicketPrice: 9000,
+};
+
+const createEventCreator = async () => {
+  const EventCreator = await ethers.getContractFactory("EventCreator");
+  const eventCreator = await EventCreator.deploy();
+  await eventCreator.deployed();
+  return eventCreator;
+};
+
+const createEvent = async (eventCreator: any, evt?: any) => {
+  const Event = await ethers.getContractFactory("Event");
+  const eventObj = evt || defaultEvent;
+  await (await eventCreator.createEvent(eventObj)).wait();
+  const eventAddress = await eventCreator.events(0);
+  return Event.attach(eventAddress);
+};
+
+describe("EventCreator", function () {
+  it("Should create new event contract", async function () {
+    const eventCreator = await createEventCreator();
+    const eventContract = await createEvent(eventCreator);
+    for (const key in defaultEvent) {
+      // @ts-ignore
+      const val = await eventContract[key]();
+      const isBn = typeof val.toNumber === "function";
+      // @ts-ignore
+      expect(isBn ? val.toNumber() : val).toBe(defaultEvent[key]);
+    }
+  });
+});
+
+describe("Event", function () {
+  it("Should create new event contract", async function () {
+    const eventCreator = await createEventCreator();
+    const eventContract = await createEvent(eventCreator);
+    for (const key in defaultEvent) {
+      // @ts-ignore
+      const val = await eventContract[key]();
+      const isBn = typeof val.toNumber === "function";
+      // @ts-ignore
+      expect(isBn ? val.toNumber() : val).toBe(defaultEvent[key]);
+    }
+  });
+});
