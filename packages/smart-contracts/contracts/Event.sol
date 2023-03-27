2beb7a6d7df7811c49837e33d0173dbab73005a7
Varik Matevosyan <varikmatevosyan@gmail.com>
2023-03-27 21:14:39 +0400

    fix(contracts): fixed small issues, added comments

--- packages/smart-contracts/contracts/Event.sol
+++ packages/smart-contracts/contracts/Event.sol
@@ -1,67 +1,72 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+// Importing the necessary modules
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
-import 'hardhat/console.sol';
 import 'erc721a-sbt/ERC721A-SBT.sol';
-import 'hardhat/console.sol';
 
+// Interface for the Event Creator
 contract IEventCreator {
   string public tokenUriServer;
 }
 
+// Library for event-related data structures
 library Utils {
+  // Struct for creating an event
   struct EventStruct {
-    uint32 maxParticipants;
-    uint256 registrationEnd;
-    uint256 start;
-    uint256 end;
-    uint256 ticketPrice;
-    uint256 preSaleTicketPrice;
-    string name;
-    string link;
-    string image;
-    string location;
-    string description;
-    bool registrationOpen;
-    bool onlyWhitelistRegistration;
+    uint32 maxParticipants; // maximum number of participants in the event
+    uint256 registrationEnd; // timestamp until when registration is open
+    uint256 start; // start time of the event
+    uint256 end; // end time of the event
+    uint256 ticketPrice; // price for tickets
+    uint256 preSaleTicketPrice; // price for pre-sale tickets
+    string name; // name of the event
+    string link; // link to the event's website
+    string image; // image of the event
+    string location; // location of the event
+    string description; // description of the event
+    bool registrationOpen; // whether registration is open or not
+    bool onlyWhitelistRegistration; // whether registration is limited to a whitelist or not
   }
 
+  // Struct for updating an event
   struct EventUpdateStruct {
-    uint32 maxParticipants;
-    uint256 registrationEnd;
-    uint256 start;
-    uint256 end;
-    uint256 ticketPrice;
-    uint256 preSaleTicketPrice;
-    string description;
-    string link;
-    string location;
-    bool registrationOpen;
-    bool onlyWhitelistRegistration;
+    uint32 maxParticipants; // maximum number of participants in the event
+    uint256 registrationEnd; // timestamp until when registration is open
+    uint256 start; // start time of the event
+    uint256 end; // end time of the event
+    uint256 ticketPrice; // price for tickets
+    uint256 preSaleTicketPrice; // price for pre-sale tickets
+    string description; // description of the event
+    string link; // link to the event's website
+    string location; // location of the event
+    bool registrationOpen; // whether registration is open or not
+    bool onlyWhitelistRegistration; // whether registration is limited to a whitelist or not
   }
 
+  // Struct for getting event information
   struct EventInfoStruct {
-    address addr;
-    address organizer;
-    bool registrationOpen;
-    bool onlyWhitelistRegistration;
-    bool isRegistered;
-    bool isChecked;
-    uint32 maxParticipants;
-    uint256 registrationEnd;
-    uint256 start;
-    uint256 end;
-    uint256 ticketPrice;
-    uint256 preSaleTicketPrice;
-    uint256 registeredParticipantCount;
-    uint256 checkedParticipantCount;
-    string description;
-    string link;
-    string name;
-    string image;
-    string location;
+    address addr; // address of the event contract
+    address organizer; // address of the event organizer
+    bool registrationOpen; // whether registration is open or not
+    bool onlyWhitelistRegistration; // whether registration is limited to a whitelist or not
+    bool isRegistered; // whether the user is registered for the event or not
+    bool isChecked; // whether the user has checked in for the event or not
+    uint32 maxParticipants; // maximum number of participants in the event
+    uint256 registrationEnd; // timestamp until when registration is open
+    uint256 start; // start time of the event
+    uint256 end; // end time of the event
+    uint256 ticketPrice; // price for tickets
+    uint256 preSaleTicketPrice; // price for pre-sale tickets
+    uint256 registeredParticipantCount; // number of participants registered for the event
+    uint256 checkedParticipantCount; // number of participants checked in for the event
+    string description; // description of the event
+    string link; // link to the event's website
+    string name; // name of the event
+    string image; // image of the event
+    string location; // location of the event
   }
 }
 
@@ -113,15 +118,26 @@ contract Event is Ownable, ERC721A_SBT {
   event ParticipantRegistered(address participant);
   event ParticipantChecked(address participant);
 
+  /**
+   * @dev Constructor function to initialize an instance of the contract with provided data.
+   * @param eventData An object of the struct containing the event data.
+   * @param owner The address that should be set as the owner of the contract.
+   * @param fee The fee percentage that should be sent to the parent contract.
+   * Requirements:
+   * - `eventData.start` and `eventData.end` should be greater than zero.
+   * - `eventData.registrationEnd` should be greater than or equal to zero.
+   */
   constructor(
     Utils.EventStruct memory eventData,
     address owner,
     uint8 fee
   ) ERC721A_SBT(eventData.name, 'CREV') {
+    // Check if required arguments are provided
     if (eventData.start == 0 || eventData.end == 0) {
       revert ProvideRequiredArguments();
     }
 
+    // Set contract data from the provided `eventData` struct
     description = eventData.description;
     link = eventData.link;
     image = eventData.image;
@@ -134,18 +150,25 @@ contract Event is Ownable, ERC721A_SBT {
     registrationOpen = eventData.registrationOpen;
     onlyWhitelistRegistration = eventData.onlyWhitelistRegistration;
 
+    // Set `registrationEnd` based on provided or `start` value
     if (eventData.registrationEnd == 0) {
       registrationEnd = eventData.start;
     } else {
       registrationEnd = eventData.registrationEnd;
     }
 
+    // Set parent contract address and fee
     parentContractAddr = msg.sender;
     parentContractFee = fee;
 
+    // Transfer ownership to `owner`
     transferOwnership(owner);
   }
 
+  /**
+   * @dev Modifier to check if registration is currently open for the event
+   * @return Throws an error if registration is not open or if the caller is already registered or if the event has already started or if no seats are available.
+   */
   modifier isRegistrationOpen() {
     uint8 status = getRegistrationOpen(block.timestamp);
     if (status == 1) revert EventAlreadyStarted();
@@ -155,6 +178,16 @@ contract Event is Ownable, ERC721A_SBT {
     _;
   }
 
+  /**
+   * @dev Function to get the current registration status of the caller
+   * @param currTimestamp The current timestamp to check registration status against
+   * @return Returns an integer representing the registration status:
+   * - 0: registration is open and caller is not registered
+   * - 1: event has already started
+   * - 2: registration is closed
+   * - 3: caller is already registered
+   * - 4: no more seats are available
+   */
   function getRegistrationOpen(
     uint256 currTimestamp
   ) public view returns (uint8) {
@@ -172,6 +205,12 @@ contract Event is Ownable, ERC721A_SBT {
     _;
   }
 
+  /**
+   * @dev Registers the given address as a participant in the event by setting their `participants` mapping to true.
+   * Also increments the `registeredParticipantCount` variable and mints a new NFT to the participant.
+   * @param addr The address to register as a participant.
+   * @private
+   */
   function _register(address addr) private {
     participants[addr] = true;
     registeredParticipantCount += 1;
@@ -179,15 +218,24 @@ contract Event is Ownable, ERC721A_SBT {
     emit ParticipantRegistered(addr);
   }
 
+  /**
+   * @dev Allows anyone to register for the event by sending the `ticketPrice` amount of ether to the contract.
+   * Reverts if `onlyWhitelistRegistration` is set to true or if the value sent is less than `ticketPrice`.
+   */
   function publicRegister() external payable isRegistrationOpen {
     if (onlyWhitelistRegistration) revert AddressNotWhitelisted();
     if (msg.value < ticketPrice) revert ValueNotEnough();
     _register(msg.sender);
   }
 
+  /**
+   * @dev Allows a whitelisted participant to register for the event by proving their address in the Merkle tree.
+   * @param _proof An array of bytes32 values representing the proof needed to verify the participant's address in the Merkle tree.
+   */
   function whitelistRegister(
     bytes32[] calldata _proof
   ) external payable isRegistrationOpen {
+    // check if only whitelist registration is enabled and if the participant's address is whitelisted
     if (
       !onlyWhitelistRegistration ||
       !MerkleProof.verify(
@@ -196,19 +244,31 @@ contract Event is Ownable, ERC721A_SBT {
         keccak256(abi.encodePacked(msg.sender))
       )
     ) revert AddressNotWhitelisted();
+    // check if the participant has sent enough funds for the pre-sale ticket
     if (msg.value < preSaleTicketPrice) revert ValueNotEnough();
+    // register the participant
     _register(msg.sender);
   }
 
+  /**
+   * @dev Allows moderators to check in a participant for the event.
+   * @param participant The address of the participant to check in.
+   */
   function checkIn(address participant) external onlyModerators {
     uint256 currTimestamp = block.timestamp;
+    // Check if event has started and has not ended
     if (currTimestamp < start || currTimestamp > end)
       revert CheckInNotAvailable();
+    // Check if participant is registered for the event
     if (!participants[participant]) revert NotRegistered();
+    // Check if participant has already checked in
     if (checkedParticipants[participant]) revert AlreadyCheckedIn();
 
+    // Set participant as checked in and increment checked participant count
     checkedParticipants[participant] = true;
     checkedParticipantCount += 1;
+
+    // Emit event to indicate that participant has been checked in
     emit ParticipantChecked(participant);
   }
 
@@ -216,15 +276,19 @@ contract Event is Ownable, ERC721A_SBT {
     moderators[addr] = status;
   }
 
+  // Allows the owner to update the event information
   function changeEventInfo(
     Utils.EventUpdateStruct memory eventData
   ) external onlyOwner {
+    // Check if the event has already started
     if (eventData.start < block.timestamp) revert CanNotModifyStartedEvent();
+    // Check if the new maxParticipants value is less than the number of registered participants
     if (
       eventData.maxParticipants < registeredParticipantCount ||
       eventData.start > eventData.end
     ) revert BadRequest();
 
+    // Update the event information
     description = eventData.description;
     link = eventData.link;
     location = eventData.location;
@@ -242,28 +306,31 @@ contract Event is Ownable, ERC721A_SBT {
     whitelistRoot = rootHash;
   }
 
+  // This function returns information about the event
+  // at the current timestamp, including event details
+  // and information about the user's registration status.
   function getEventInfo(
-    uint256 currTimestamp
+    uint256 currTimestamp // current timestamp to check registration status
   ) external view returns (Utils.EventInfoStruct memory) {
     Utils.EventInfoStruct memory evt;
-    evt.start = start;
-    evt.end = end;
-    evt.registrationEnd = registrationEnd;
-    evt.name = name();
-    evt.description = description;
-    evt.maxParticipants = maxParticipants;
-    evt.ticketPrice = ticketPrice;
-    evt.preSaleTicketPrice = preSaleTicketPrice;
-    evt.link = link;
-    evt.image = image;
-    evt.location = location;
-    evt.organizer = owner();
-    evt.registrationOpen = getRegistrationOpen(currTimestamp) == 0;
-    evt.isRegistered = participants[msg.sender];
-    evt.isChecked = checkedParticipants[msg.sender];
-    evt.registeredParticipantCount = registeredParticipantCount;
-    evt.checkedParticipantCount = checkedParticipantCount;
-    evt.addr = address(this);
+    evt.start = start; // event start timestamp
+    evt.end = end; // event end timestamp
+    evt.registrationEnd = registrationEnd; // registration end timestamp
+    evt.name = name(); // event name
+    evt.description = description; // event description
+    evt.maxParticipants = maxParticipants; // maximum number of participants
+    evt.ticketPrice = ticketPrice; // ticket price in main sale
+    evt.preSaleTicketPrice = preSaleTicketPrice; // ticket price in pre-sale
+    evt.link = link; // event link
+    evt.image = image; // event image
+    evt.location = location; // event location
+    evt.organizer = owner(); // event organizer
+    evt.registrationOpen = getRegistrationOpen(currTimestamp) == 0; // registration status
+    evt.isRegistered = participants[msg.sender]; // user registration status
+    evt.isChecked = checkedParticipants[msg.sender]; // user check-in status
+    evt.registeredParticipantCount = registeredParticipantCount; // number of registered participants
+    evt.checkedParticipantCount = checkedParticipantCount; // number of checked-in participants
+    evt.addr = address(this); // event contract address
 
     return evt;
   }
@@ -299,14 +366,23 @@ contract Event is Ownable, ERC721A_SBT {
     return 1;
   }
 
+  /**
+   * @dev Returns the URI for a given token ID. Throws an error if the token does not exist.
+   * @param tokenId The ID of the token to query.
+   * @return A string representing the token URI.
+   */
   function tokenURI(
     uint256 tokenId
   ) public view virtual override returns (string memory) {
+    // Check if token exists
     if (!_exists(tokenId)) {
       revert URIQueryForNonexistentToken();
     }
 
+    // Get the base URI from the parent contract
     string memory tokenUri = IEventCreator(parentContractAddr).tokenUriServer();
+
+    // Generate the complete token URI
     bytes memory encodedData = abi.encodePacked(
       tokenUri,
       Strings.toString(block.chainid),
--- packages/smart-contracts/contracts/EventCreator.sol
+++ packages/smart-contracts/contracts/EventCreator.sol
@@ -1,53 +1,84 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import 'hardhat/console.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import './Event.sol';
 
+// Contract for creating and managing events
 contract EventCreator is Ownable {
+  // The fee that will be paid from event contract
   uint8 public fee = 1;
-  string public tokenUriServer = 'https://metadata.cryvents.xyz/';
+
+  // The server used to get the token URI for an event
+  string public tokenUriServer = 'https://metadata.crevents.xyz/';
+
+  // An array of all the event contract addresses created by this contract
   address[] public events;
 
+  // Event emitted when an event is created
   event EventCreated(address addr);
 
+  // Event emitted when fee is received
+  event Received(address, uint);
+
+  // Function for creating a new event contract
   function createEvent(
     Utils.EventStruct calldata _eventData
   ) external returns (address) {
+    // Create a new Event contract with the given data and the sender's address and fee
     address eventContractAddress = address(
       new Event(_eventData, msg.sender, fee)
     );
+
+    // Add the new event contract's address to the events array
     events.push(eventContractAddress);
+
+    // Emit an EventCreated event with the new event contract's address
     emit EventCreated(eventContractAddress);
+
+    // Return the new event contract's address
     return eventContractAddress;
   }
 
-  // whithdraw contract balance
+  // Function for withdrawing the contract's balance, only callable by the contract owner
   function withdraw() external onlyOwner {
+    // Get the current balance of the contract
     uint256 contractBalance = address(this).balance;
+
+    // If the contract balance is 0, throw an exception
     if (contractBalance == 0) revert TransferFailed();
 
-    payable(owner()).transfer(contractBalance);
+    // Transfer the contract balance to the owner
+    (success, ) = payable(owner()).call{value: contractBalance}('');
+
+    if (!success) revert TransferFailed();
   }
 
-  // change tokenUriServer
+  receive() external payable {
+    emit Received(msg.sender, msg.value);
+  }
+
+  // Function for changing the token URI server, only callable by the contract owner
   function setTokenUri(string memory uri) external onlyOwner {
+    // Set the new token URI server
     tokenUriServer = uri;
   }
 
-  // getting events with offset, limit in reversed order
+  // Function for getting an array of event info structs, with an offset, limit, and current timestamp
   function getEvents(
     uint256 offset,
     uint256 limit,
     uint256 currTimestamp
   ) external view returns (Utils.EventInfoStruct[] memory) {
+    // Get the maximum length of the events array
     uint256 maxLen = events.length;
+
+    // Initialize variables for the result array length, iteration limit, and reversed index
     uint256 arrLength;
     uint256 iterLimit;
     uint256 reversedIndex;
 
-    // getting the length for result array
+    // Determine the length of the result array based on the offset and limit
     if (offset >= maxLen) {
       arrLength = 0;
     } else if ((maxLen - offset) < limit) {
@@ -56,18 +87,21 @@ contract EventCreator is Ownable {
       arrLength = limit;
     }
 
+    // Create an array for the result event info structs
     Utils.EventInfoStruct[] memory res = new Utils.EventInfoStruct[](arrLength);
 
+    // If the result array length is 0, return the empty array
     if (arrLength == 0) return res;
 
-    // getting the max iteration index
+    // Determine the iteration limit for populating the result array
     iterLimit = maxLen - arrLength;
 
-    // getting reversed index
+    // Determine the reversed index for populating the result array
     if (arrLength > 0) {
       reversedIndex = arrLength - 1;
     }
 
+    // Iterate through the events array in reverse order, and populate the result array with the event info structs
     uint256 i = reversedIndex;
 
     while (true) {
